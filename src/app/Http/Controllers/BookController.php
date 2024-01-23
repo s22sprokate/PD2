5c49aa799f16d56290b6c9b76567f48ab7547070
s22sprokate <s22sprokate@venta.lv>
2024-01-23 14:34:20 +0200

    save

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Author;
 
 class BookController extends Controller
 {
@@ -18,4 +19,80 @@ class BookController extends Controller
             ]
         );
     }
+
+    public function create()
+    {
+        $authors = Author::orderBy('name', 'asc')->get();
+        return view(
+            'book.form',
+            [
+                'title' => 'Add book',
+                'book' => new Book(),
+                'authors' => $authors,
+            ]
+        );
+    }
+
+    public function put(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|min:3|max:256',
+            'author_id' => 'required',
+            'description' => 'nullable',
+            'price' => 'nullable|numeric',
+            'year' => 'numeric',
+            'image' => 'nullable|image',
+            'display' => 'nullable'
+        ]);
+        $book = new Book();
+        $book->name = $validatedData['name'];
+        $book->author_id = $validatedData['author_id'];
+        $book->description = $validatedData['description'];
+        $book->price = $validatedData['price'];
+        $book->year = $validatedData['year'];
+        $book->display = (bool) ($validatedData['display'] ?? false);
+        $book->save();
+
+        return redirect('/books');
+    }
+
+    public function update(Book $book)
+    {
+        $authors = Author::orderBy('name', 'asc')->get();
+        return view(
+            'book.form',
+            [
+                'title' => 'Edit book',
+                'book' => $book,
+                'authors' => $authors,
+            ]
+        );
+    }
+    public function patch(Book $book, Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|min:3|max:256',
+            'author_id' => 'required',
+            'description' => 'nullable',
+            'price' => 'nullable|numeric',
+            'year' => 'numeric',
+            'image' => 'nullable|image',
+            'display' => 'nullable'
+        ]);
+        $book->name = $validatedData['name'];
+        $book->author_id = $validatedData['author_id'];
+        $book->description = $validatedData['description'];
+        $book->price = $validatedData['price'];
+        $book->year = $validatedData['year'];
+        $book->display = (bool) ($validatedData['display'] ?? false);
+        $book->save();
+        return redirect('/books/update/' . $book->id);
+    }
+
+    public function delete(Book $book)
+    {
+        $book->delete();
+        return redirect('/books');
+    }
+
 }
--- src/app/Models/Book.php
+++ src/app/Models/Book.php
@@ -9,21 +9,6 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function up()
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('author_id');
-            $table->string('name', 256);
-            $table->text('description')->nullable();
-            $table->decimal('price', 8, 2)->nullable();
-            $table->integer('year');
-            $table->string('image', 256)->nullable();
-            $table->boolean('display');
-            $table->timestamps();
-        });
-    }
-
     public function author()
     {
         return $this->belongsTo(Author::class);
